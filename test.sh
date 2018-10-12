1f092d51cbd704bb56652119916facd8810872eb
Matthias Hryniszak <padcom@gmail.com>
2018-10-12 09:02:32 +0200

    Added content in multiple versions

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-curl --verbose -X POST -H 'Version: 1' --data-binary '@pom.xml' http://localhost:8080/baibulo-java-example/assets/pom.xml
-curl --verbose -H 'Version: 1' http://localhost:8080/baibulo-java-example/assets/pom.xml
+curl --verbose -X POST -H 'Version: 1' --data-binary '@content/version-1/styles.css' http://localhost:8080/baibulo-java-example/assets/styles.css
+curl --verbose -X POST -H 'Version: 2' --data-binary '@content/version-2/styles.css' http://localhost:8080/baibulo-java-example/assets/styles.css
+curl --verbose -X POST -H 'Version: release' --data-binary '@content/release/styles.css' http://localhost:8080/baibulo-java-example/assets/styles.css
